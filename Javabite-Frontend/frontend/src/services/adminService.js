13a24ba9d7f8bdb89d67c31a88ea1726a9436976
Tejas_patil07 <tejasrpatil804@gmail.com>
2026-01-08 09:25:09 +0530

    Delete Javabite-Frontend directory

--- Javabite-Frontend/frontend/src/services/adminService.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axiosClient";
-
-export const inviteStaff = async (data) => {
-  return axiosClient.post("/api/admin/staff/invite", data);
-};
-
-export const getInvites = async () => {
-  return axiosClient.get("/api/admin/staff/invites");
-};
--- Javabite-Frontend/frontend/src/services/menuService.js
+++
@@ -1,5 +0,0 @@
-import axiosClient from "./axiosClient";
-
-export const getMenu = () => {
-  return axiosClient.get("/menu/all");
-};
